3ed20583ac6cb53a5cd62b334180b56ecb38fd35
Jody Neckles <jody@technative.tv>
2018-10-18 12:14:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Beatles: 'Yesterday',
   ['Freddy Mercury']: ['Notorious BIG']
 };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
